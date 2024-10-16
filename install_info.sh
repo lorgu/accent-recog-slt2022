e4bf5d2e45ffc71844b4bf85f1429d2150de2b7e
Lorenz Gutscher <81177675+lorgu@users.noreply.github.com>
2024-10-16 09:40:10 +0200

    Update install_info.sh
    dir change (nas to home changed)

--- install_info.sh
+++ install_info.sh
@@ -4,14 +4,14 @@ chmod +x Miniconda3-latest-Linux-x86_64.sh && \
 export PATH="$HOME/miniconda/bin:$PATH" && \
 conda create -n dicla python=3.10 -y && \
 source $HOME/miniconda/bin/activate dicla && \
-mkdir /nas && \
-mkdir /nas/projects && \
-mkdir /nas/projects/vokquant && \
-mkdir /nas/projects/vokquant/data && \
-mkdir /nas/projects/vokquant/data/dicla && \
-git clone https://github.com/lorgu/accent-recog-slt2022 /nas/projects/vokquant/accent-recog-slt2022 && \
-cd /nas/projects/vokquant/accent-recog-slt2022/ && \
+mkdir /home && \
+mkdir /home/projects && \
+mkdir /home/projects/vokquant && \
+mkdir /home/projects/vokquant/data && \
+mkdir /home/projects/vokquant/data/dicla && \
+git clone https://github.com/lorgu/accent-recog-slt2022 /home/projects/vokquant/accent-recog-slt2022 && \
+cd /home/projects/vokquant/accent-recog-slt2022/ && \
 pip install -r requirements.txt && \
 pip install torchtext==0.13.1 torch==1.12.1+cu116 torchvision==0.13.1+cu116 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu116 && \
 apt-get update && apt-get install -y pigz && \
-tar --use-compress-program=pigz -xf /workspace/processed_16khz_renamed.tar.gz -C /nas/projects/vokquant/data/dicla/
+tar --use-compress-program=pigz -xf /workspace/processed_16khz_renamed.tar.gz -C /home/projects/vokquant/data/dicla/
