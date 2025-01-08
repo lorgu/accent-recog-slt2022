a083de364c6e0ae85829f83f1f9ebd5bea109db0
Lorenz Gutscher <81177675+lorgu@users.noreply.github.com>
2025-01-08 13:30:31 +0100

    Update install_info.sh
    path adjustments

--- install_info.sh
+++ install_info.sh
@@ -4,10 +4,10 @@ chmod +x Miniconda3-latest-Linux-x86_64.sh && \
 export PATH="$HOME/miniconda/bin:$PATH" && \
 conda create -n dicla python=3.10 -y && \
 source $HOME/miniconda/bin/activate dicla && \
-mkdir -p /home/projects/vokquant/data/dicla && \
+mkdir -p /home/projects/vokquant/data/dicla/augmented_ofai_cv/ && \
 git clone https://github.com/lorgu/accent-recog-slt2022 /home/projects/vokquant/accent-recog-slt2022 && \
 cd /home/projects/vokquant/accent-recog-slt2022/ && \
 pip install -q -r requirements.txt && \
 pip install -q torchtext==0.13.1 torch==1.12.1+cu116 torchvision==0.13.1+cu116 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu116 && \
 apt-get update && apt-get install -y pigz && \
-tar --use-compress-program=pigz -xf /home/projects/vokquant/data/at_augmented/at_augmented.tar.gz -C /home/projects/vokquant/data/dicla/augmented_ofai_cv/
+tar --use-compress-program=pigz -xf /home/projects/vokquant/data/at_augmented_ofai_cv/at_augmented_ofai_cv.tar.gz -C /home/projects/vokquant/data/dicla/augmented_ofai_cv/
